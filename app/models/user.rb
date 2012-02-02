71630ece8ea45505cb0bf89492f22a29a018e9c2
Artem Shoobovych <shybovycha@gmail.com>
2012-02-03 00:04:48 +0200

    Added some forms and validations

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+require 'digest/md5'
+
+class User < ActiveRecord::Base
+	validates :email, 
+		:length => { :minimum => 3, :maximum => 255 },
+		:uniqueness => true,
+		:presence => true,
+		:email => true,
+		:on => :create
+
+	validates :name, 
+		:uniqueness => true,
+		:presence => true,
+		:length => { :minimum => 3, :maximum => 255 },
+		:on => :create
+
+	validates :password,
+		:length => { :minimum => 6, :maximum => 255 },
+		:on => :create,
+		:presence => true,
+		:confirmation => true
+
+public
+  def self.authenticate(email, password)
+    return false if email.nil? or password.nil? 
+    return false if email.empty? or password.empty?
+
+    u = User.find_by_email(email)
+
+    return false if u.nil?
+    return false if Digest::MD5.hexdigest(password) != u.password
+    return true
+  end
+end
