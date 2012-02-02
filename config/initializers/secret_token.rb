115d8bee5ddbe5ce81f80874f73e27dab55305e8
Artem Shoobovych <shybovycha@gmail.com>
2012-02-02 21:26:12 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moolab::Application.config.secret_token = '7ae215a79ad5083370d6f1c7babb36ea2620cbe33e673ad719a05ed42ee2b05514e31436e426e5fc6e9147507d286a589667d9b54e01dda7280db74a85fef89d'
