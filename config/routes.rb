fcf6f52fee6d3851f1702eee74294e94c0d8bd9b
Artem Shoobovych <shybovycha@gmail.com>
2012-02-03 00:04:33 +0200

    Added some forms and validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Moolab::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/register"
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Moolab::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
